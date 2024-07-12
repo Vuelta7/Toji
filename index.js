4ac0c1c17fec8f652b4f0e59dbdf33ab4c2e0f30
Vuelta7 <158291660+Vuelta7@users.noreply.github.com>
2024-07-12 21:06:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// let x = 0;
+// let firstname = ["Uriel", "Syle", "johann", "Kyle"];
+// let lastname = ["Vuelta", "Eusebio", "Francisco", "Cruz"];
+// let age = [16, 17, 18, 16];
+// let grade = [
+//   [99, 99, 99],
+//   [95, 95, 95],
+//   [90, 90, 90],
+//   [75, 75, 75],
+// ];
+
+// console.log(`index: ${x}`);
+// console.log(`First Name: ${firstname[x]}`);
+// console.log(`Last Name: ${lastname[x]}`);
+// console.log(`Age: ${age[x]}`);
+
+// let english = grade[x][0];
+// let math = grade[x][1];
+// let filipino = grade[x][2];
+// let average = (math + filipino + english) / 3;
+
+// console.log(`Your gade is ${average}`);
+// if (average > 101) {
+//   console.log("You're Grade is invalid");
+// } else if (average >= 98) {
+//   console.log("You're With Highest Honors");
+// } else if (average >= 95) {
+//   console.log("You're With High Honors");
+// } else if (average >= 90) {
+//   console.log("You're With Honors");
+// } else if (average >= 75) {
+//   console.log("You're Passed");
+// } else {
+//   console.log("You're Failed");
+// }
+
+// let fullName = "Uriel";
+// let age = 17;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+// let username = window.prompt("What is your name?");
+
+// let userName;
+
+// document.getElementById("mySubmit").onclick = function () {
+//   userName = document.getElementById("myText").value;
+//   document.getElementById("p1").textContent = `Hello ${userName}`;
+// };
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// document.getElementById("mySubmit").onclick = function () {
+//   radius = document.getElementById("myText").value;
+//   radius = Number(radius);
+//   circumference = 2 * PI * radius;
+//   document.getElementById("p1").textContent = circumference + "cm";
+// };
